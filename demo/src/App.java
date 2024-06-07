b9e4e30788a0039b6e4bae78b9f71069b20c0cd5
sergeyzoloto <sergiozoloto@gmail.com>
2024-06-07 17:28:32 +0200

    escape sequences and comments

--- demo/src/App.java
+++ demo/src/App.java
@@ -1,5 +1,15 @@
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    System.out.println("I love lasagna!");
+    System.out.print("I love pizza!\n");
+    System.out.println("Peace, love and other shit.\n");
+    System.out.println("\tI feel good.\n");
+    System.out.println("\"Quotes\".");
+    // This is a comment
+    /*
+     * This is a long comment.
+     * It's multiline. 
+     */
+  }
 }
