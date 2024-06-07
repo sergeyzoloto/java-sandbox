330d2e5635b6f1e724bf0f791e75186ba69b4f43
sergeyzoloto <sergiozoloto@gmail.com>
2024-06-07 18:06:44 +0200

    user input

---
+++ demo/src/Input.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Input {
+  
+  public static void main(String[] args) {
+    
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter your name: ");
+    String name = scanner.nextLine();
+
+    System.out.println("Enter your age: ");
+    int age = scanner.nextInt();
+    
+    scanner.nextLine(); // handle next line character (enter key, \n)
+
+    System.out.println("What is your favorite food: ");
+    String food = scanner.nextLine(); // called after nextInt()
+
+    System.out.println("Hello " + name);
+    System.out.println("You are " + age + " years old.");
+    System.out.println("You like " + food);
+
+  }
+
+}
