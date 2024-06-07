1f98dbf5a2d5391b5aef55543d3fa5ebcd7e18a0
sergeyzoloto <sergiozoloto@gmail.com>
2024-06-07 17:50:48 +0200

    variables

--- demo/src/App.java
+++ demo/src/App.java
@@ -11,5 +11,35 @@ public class App {
      * This is a long comment.
      * It's multiline. 
      */
+    /*
+     * Variables
+     */
+
+     int x = 123;
+     System.out.println("x number is " + x);
+
+     int y; // declaration
+     y = 456; // assignment
+
+     int z = 789; // initialization
+
+     System.out.println("y number is " + y);
+     System.out.println("z number is " + z);
+     
+     long l = 123456789123456789L;
+
+     System.out.println("l number is " + l);
+
+     double d = 3.14;
+     boolean b = true;
+
+     System.out.println("d number is " + d);
+     System.out.println("b number is " + b);
+
+     char symbol = '@';
+     String name = "Name";
+     
+     System.out.println("symbol number is " + symbol);
+     System.out.println("name variable is " + name);
   }
 }
