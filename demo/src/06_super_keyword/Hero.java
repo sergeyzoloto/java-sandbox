8bacebda0cf01ac32a29c05733a571f24e2ad1b2
sergeyzoloto <sergiozoloto@gmail.com>
2024-06-16 16:14:36 +0200

    super keyword

---
+++ demo/src/06_super_keyword/Hero.java
@@ -0,0 +1,13 @@
+public class Hero extends Person{
+
+  String power;
+
+  public Hero(String name, int age, String power) {
+    super(name, age);
+    this.power = power;
+  }
+
+  public String toString() {
+    return super.toString() + "\nPower: " + power;
+  }
+}
