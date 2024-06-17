dcb3784d55cf76bf879d76ff91bb6139a673540f
sergeyzoloto <sergiozoloto@gmail.com>
2024-06-17 18:40:42 +0200

    JFileChooser

---
+++ demo/src/GUI/JFileChooser/Main.java
@@ -0,0 +1,12 @@
+package GUI.JFileChooser;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+      // JFileChooser = A GUI mechanism that let's a user choose a file
+
+      new MyFrame();
+    }
+  
+}
